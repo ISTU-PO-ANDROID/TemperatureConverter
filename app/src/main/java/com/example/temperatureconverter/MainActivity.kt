6de79e373a6ddf5ab26737a1736dd79d165e7226
ISTU-PO-ANDROID <istu.po.2204@gmail.com>
2025-01-07 15:48:03 +0400

    feat: Header added

--- app/src/main/java/com/example/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.temperatureconverter.ui.theme.TemperatureConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,50 +22,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Column {
-                Hello("everyone")
-                Hello("friend")
-            }
-
-//            Text("Hello friends! Welcome to Jetpack Compose")
-//            TemperatureConverterTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//            }
+            MainActivityContent()
         }
     }
 }
 
 @Composable
-fun Hello(name: String) {
-    Text("Hello $name! Welcome to Jetpack Compose.")
+fun Header(image: Int, description: String) {
+    Image(
+        painter = painterResource(image),
+        contentDescription = description,
+        modifier = Modifier
+            .height(180.dp)
+            .fillMaxWidth(),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+fun TemperatureText(celsius: Int) {
+    val fahrenheit = (celsius.toDouble()*9/5)+32
+    Text("$celsius Celsius is $fahrenheit Fahrenheit")
+}
+
+@Composable
+fun MainActivityContent() {
+    Column {
+        Header(R.drawable.sunrise, "sunrise image")
+        TemperatureText(0)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainActivity() {
-    Column {
-        Hello("friends")
-        Hello("everyone")
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TemperatureConverterTheme {
-        Greeting("Android")
-    }
+    MainActivityContent()
 }
