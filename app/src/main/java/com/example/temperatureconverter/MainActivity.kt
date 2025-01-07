06bfa8e44259253668145319de91866e7324384e
ISTU-PO-ANDROID <istu.po.2204@gmail.com>
2025-01-07 15:18:34 +0400

    feat: simple app with compose

---
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.temperatureconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.temperatureconverter.ui.theme.TemperatureConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Column {
+                Hello("everyone")
+                Hello("friend")
+            }
+
+//            Text("Hello friends! Welcome to Jetpack Compose")
+//            TemperatureConverterTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+        }
+    }
+}
+
+@Composable
+fun Hello(name: String) {
+    Text("Hello $name! Welcome to Jetpack Compose.")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMainActivity() {
+    Column {
+        Hello("friends")
+        Hello("everyone")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TemperatureConverterTheme {
+        Greeting("Android")
+    }
+}
