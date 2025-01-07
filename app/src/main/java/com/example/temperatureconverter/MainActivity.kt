6058acf5b6b07eef4b91c825291e6f5fb1e68076
ISTU-PO-ANDROID <istu.po.2204@gmail.com>
2025-01-07 16:13:40 +0400

    feat: EnterTemperature added

--- app/src/main/java/com/example/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.temperatureconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TemperatureText(celsius: Int) {
-    val fahrenheit = (celsius.toDouble()*9/5)+32
+    val fahrenheit = (celsius.toDouble() * 9 / 5) + 32
     Text("$celsius Celsius is $fahrenheit Fahrenheit")
 }
 
@@ -40,6 +40,16 @@ fun ConvertButton(clicked: () -> Unit) {
     Button(onClick = clicked) {
         Text("Convert")
     }
+}
+
+@Composable
+fun EnterTemperature(temperature: String, changed: (String) -> Unit) {
+    TextField(
+        value = temperature,
+        label = { Text("Enter a temperature in Celsius") },
+        onValueChange = changed,
+        modifier = Modifier.fillMaxWidth()
+    )
 }
 
 @Composable
@@ -54,12 +64,20 @@ fun Header(image: Int, description: String) {
     )
 }
 
+
 @Composable
 fun MainActivityContent() {
     val celsius = remember { mutableStateOf(0) }
+    val newCelsius = remember { mutableStateOf("") }
+
     Column {
         Header(R.drawable.sunrise, "sunrise image")
-        ConvertButton { celsius.value = 20 }
+        EnterTemperature(newCelsius.value) { newCelsius.value = it }
+        ConvertButton {
+            newCelsius.value.toIntOrNull()?.let {
+                celsius.value = it
+            }
+        }
         TemperatureText(celsius.value)
     }
 }
